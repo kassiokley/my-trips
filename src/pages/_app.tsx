a12584acbf92b248300ade1b9edc2064e469e492
kassiokley <seloseishiro@gmail.com>
2021-06-04 00:55:01 -0300

    modifica estrutura inicial do boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/images/icon-512.png" />
         <link rel="apple-touch-icon" href="/images/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+          content="A simple project to show my favorite spots in the world."
         />
       </Head>
       <GlobalStyles />
